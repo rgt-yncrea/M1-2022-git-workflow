d1c485ee2899a16dbf494390b29ee72a2877bf16
tigerboyeli <eliphetepierre1999@gmail.com>
2022-09-29 13:04:34 +0200

    mon setup

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
 from setuptools import find_packages
 
-#with open('README.md') asf:
-#readme = f.read()
 
 from pathlib import Path
 this_directory = Path(__file__).parent
