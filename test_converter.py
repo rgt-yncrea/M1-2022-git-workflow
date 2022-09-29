233c4ae0974d5f3526e47f63083cdf6180801651
Tomitom13 <thomas.buton@isen.yncrea.fr>
2022-09-29 13:50:34 +0200

    correct source

--- test_converter.py
+++ test_converter.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from scr.thomas_BUTON.converter import dec_to_bin, bin_to_dec
+from src.thomas_BUTON.converter import dec_to_bin, bin_to_dec
 
 
 class TestConverter(TestCase):
