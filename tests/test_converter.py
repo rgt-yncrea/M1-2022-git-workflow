70ceb860f1a6c717be8107fda76cce27f5ebaeb4
Bajiio <60874928+Bajiio@users.noreply.github.com>
2022-09-29 11:04:05 +0200

    try fixing bug import

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
+import sys
+sys.path.append('../')
 
-from tp2_git.src.converter_package_Bajiio.converter import dec_to_bin, bin_to_dec
+from src.converter_package_Bajio.converter import dec_to_bin, bin_to_dec
 
 
 class TestConverter(TestCase):
