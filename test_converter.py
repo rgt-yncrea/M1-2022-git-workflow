02946e5406915cd8e75ae91c62f5df95c0ce1f56
Bajiio <60874928+Bajiio@users.noreply.github.com>
2022-09-29 10:38:54 +0200

    Update test to import right place

--- test_converter.py
+++ test_converter.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from converter import dec_to_bin, bin_to_dec
+from src/converter_package_Bajiio/converter import dec_to_bin, bin_to_dec
 
 
 class TestConverter(TestCase):
