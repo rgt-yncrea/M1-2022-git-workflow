216ad40479bfaff1f7ef8b010c2b714c85b65ad8
Bajiio <60874928+Bajiio@users.noreply.github.com>
2022-09-29 10:58:24 +0200

    fix import bug

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from converter_package_Bajiio.converter import dec_to_bin, bin_to_dec
+from tp2_git.converter_package_Bajiio.converter import dec_to_bin, bin_to_dec
 
 
 class TestConverter(TestCase):
