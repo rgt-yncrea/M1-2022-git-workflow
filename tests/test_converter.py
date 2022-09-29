7588753e2a107e557aeb7d7f207417255c8bd1d8
Tomitom13 <thomas.buton@isen.yncrea.fr>
2022-09-29 13:42:44 +0200

    another try

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,7 +1,9 @@
 from unittest import TestCase
 
-from home.runner.work.M1-2022-git-workflow.M1-2022-git-workflow.scr.thomas_BUTON.converter import dec_to_bin, bin_to_dec
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath( os.path.join(os.path.dirname(__file__), 
+                                               '../src/thomas_BUTON/') ))
 
 class TestConverter(TestCase):
     def test_dec_to_bin_even(self):
